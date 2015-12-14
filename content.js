de782d53803940d96dd541912cda514afd7703dd
Jonathan King <jonathan.j.king@gmail.com>
2015-12-14 21:37:09 +0000

    Tidying, simplifying

--- content.js
+++ content.js
@@ -1,10 +1,8 @@
 
 function showComments(cm) {
-	var divvy = '<div style="border-top:1px solid black;height:1px;width:100%;"></div>'
-	if (cm.find('commentsWindow' + window.frameCounter).length > 0){
-		return false
-	}
-	bouncing(cm, true)
+	if (commentsExist(cm)) return false
+	var containing_div = '<div style="border-top:1px solid black;height:1px;width:100%;"></div>'
+	res_comments_loading(cm, true)
 	window.frameCounter ? window.frameCounter++ : window.frameCounter = 1
 	var url = cm.find(".comments").attr("href").replace(/\/$/, '') + ".json"
 	$('<div />', {
@@ -21,10 +19,10 @@ function showComments(cm) {
 				prefix = htmlDecode(p)
 			else
 				prefix = p
-			return prefix + divvy + htmlDecode(c)
+			return prefix + containing_div + htmlDecode(c)
 		})
 		$('#commentsWindow' + window.frameCounter).html(html)
-		bouncing(cm, false)
+		res_comments_loading(cm, false)
 	})
 }
 
@@ -34,31 +32,34 @@ function htmlDecode(input){
   return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
 }
 
-function removeComments() {
-	bouncing(true)
+function removeComments(cm) {
+	if (!commentsExist(cm)) return false
+	res_comments_loading(true)
 	$('#commentsWindow' + window.frameCounter).remove()
 	window.frameCounter--
-	bouncing(false)
+	res_comments_loading(false)
 }
 
-function bouncing(cm, on) {
+function commentsExist(cm) {
+	return cm.find('#commentsWindow' + window.frameCounter).length > 0 ? true : false
+}
+
+function res_comments_loading(cm, on) {
 	if (on) {
 		window.isBouncing = true;
-		cm.append('<div id="bouncing">LOADING...</div>')
+		cm.append('<div id="res_comments_loading">LOADING...</div>')
 	} else {
-		$('#bouncing').remove()
+		$('#res_comments_loading').remove()
 		window.setTimeout(function(){ window.isBouncing = false }, 250)
 	}	
 }
 
 $(document).keypress(function(event){
-	if (event.which == 59 && !window.isBouncing) { // semicolon
-		console.log('got semicolon')
+	if (event.which == 109 && !window.isBouncing) { 
 		var cm = $(".RES-keyNav-activeElement")
 		showComments(cm)
 	}
-	if (event.which == 39 && !window.isBouncing) { // apostrophe
-		console.log('got apostrophe')
+	if (event.which == 105 && !window.isBouncing) {
 		var cm = $(".RES-keyNav-activeElement")
 		removeComments(cm)
 	}
